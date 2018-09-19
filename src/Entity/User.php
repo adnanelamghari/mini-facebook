b0eb84f6da1521b04c6597dba84572ca8524d6f2
adnane lamghari <adnane.lmg@gmail.com>
2018-09-19 01:35:31 +0100

    first commit

---
+++ src/Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+}
