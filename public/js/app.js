c4d079ef9dc94d7f05b068c88affc73b550b206e
adnane lamghari <adnane.lmg@gmail.com>
2018-09-21 02:26:52 +0100

    register

--- public/js/app.js
+++ public/js/app.js
@@ -7,8 +7,6 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
-require('@fortawesome/fontawesome-free/css/all.min.css');
-require('@fortawesome/fontawesome-free/js/all.js');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
---
+++ src/Controller/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'controller_name' => 'Login',
+        ));
+    }
+}
